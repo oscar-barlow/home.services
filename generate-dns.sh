4a16cda45efdbba286cf72d23f9837354dcc0962
Oscar Barlow <oscar-barlow@users.noreply.github.com>
2025-06-09 15:37:35 +0100

    rewrite dns generation script to make it easier to debug sed

--- generate-dns.sh
+++ generate-dns.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Generating DNS configuration..."
 
-sed "s/DOMAIN_PLACEHOLDER/${DOMAIN}/g; \
-     s/JELLYFIN_IP_PLACEHOLDER/${JELLYFIN_IP}/g; \
-     s/PIHOLE_IP_PLACEHOLDER/${PIHOLE_IP}/g; \
-   /etc/dnsmasq.d/02-custom-dns.conf.template > /etc/dnsmasq.d/02-custom-dns.conf"
+sed -e "s/DOMAIN_PLACEHOLDER/${DOMAIN}/g" \
+    -e "s/JELLYFIN_IP_PLACEHOLDER/${JELLYFIN_IP}/g" \
+    -e "s/PIHOLE_IP_PLACEHOLDER/${PIHOLE_IP}/g" \
+    -e "s/HELLO_WORLD_IP_PLACEHOLDER/${HELLO_WORLD_IP}/g" \
+    /etc/dnsmasq.d/02-custom-dns.conf.template > /etc/dnsmasq.d/02-custom-dns.conf
 
 echo "DNS configuration generated successfully."
