a4e2f131d88603b345f331c752e3028e82461a31
Oscar Barlow <oscar-barlow@users.noreply.github.com>
2025-06-09 15:29:53 +0100

    script doesn't seem to work still...

--- generate-dns.sh
+++ generate-dns.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Generating DNS configuration..."
 
 sed "s/DOMAIN_PLACEHOLDER/${DOMAIN}/g; \
      s/JELLYFIN_IP_PLACEHOLDER/${JELLYFIN_IP}/g; \
      s/PIHOLE_IP_PLACEHOLDER/${PIHOLE_IP}/g; \
-     s/HELLO_WORLD_IP_PLACEHOLDER/${HELLO_WORLD_IP}/g" \
-   /etc/dnsmasq.d/02-custom-dns.conf.template >/etc/dnsmasq.d/02-custom-dns.conf
+   /etc/dnsmasq.d/02-custom-dns.conf.template > /etc/dnsmasq.d/02-custom-dns.conf"
+
+echo "DNS configuration generated successfully."
